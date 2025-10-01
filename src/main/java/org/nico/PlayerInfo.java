3c54ba07162870762fa4b15e534c8b8b41024d44
Nico <152223501+nicobota@users.noreply.github.com>
2025-09-30 19:02:59 -0700

    oh yeah

--- src/main/java/org/nico/PlayerInfo.java
+++ src/main/java/org/nico/PlayerInfo.java
@@ -13,49 +13,72 @@ import java.util.concurrent.ExecutionException;
 public class PlayerInfo {
 
     public static final HypixelAPI api = Util.API;
+    public static PlayerReply apiReply;
+    public static GuildReply guildReply;
+    public static Player player;
+    public static Guild guild;
 
-    public static String getUUID(String s) throws InterruptedException, ExecutionException {
-        String reply = api.getPlayerByName(s).get().getPlayer().getUuid().toString();
+    public static void shutDown() {
+        api.shutdown();
+    }
+    public static String getUUID(String s) throws ExecutionException {
+        String reply = null;
+        try {
+            reply = api.getPlayerByName(s).get().getPlayer().getUuid().toString();;
+            return reply;
+        } catch (InterruptedException e) {
+            System.out.println("Couldn't get Player UUID");
+        }
         return reply;
     }
 
-    public static void getPlayerInfo() throws InterruptedException {
-        PlayerReply apiReply;
-        GuildReply guildReply;
-
+    private static String onlineStatus(UUID ID) throws InterruptedException {
         try {
-            UUID playerUuid = Util.FAKENICO;
+            apiReply = api.getPlayerByUuid(ID).get();
+            guildReply = api.getGuildByPlayer(ID).get();
+        } catch (ExecutionException e) {
+            Fails.requestFail();
+        }
 
-            apiReply = api.getPlayerByUuid(playerUuid).get();
-            guildReply = api.getGuildByPlayer(playerUuid).get();
+        return (player.isOnline()? "Online" : "Offline");
+    }
+
+    public static void getPlayerInfo(UUID ID) throws InterruptedException {
+        try {
+
+            apiReply = api.getPlayerByUuid(ID).get();
+            guildReply = api.getGuildByPlayer(ID).get();
 
         } catch (ExecutionException e) {
-            System.err.println("API Request Failed");
+            Fails.requestFail();
 
             e.getCause().printStackTrace();
             return;
 
-        } finally {
-            api.shutdown();
         }
 
-        Player player = apiReply.getPlayer();
-        Guild guild = guildReply.getGuild();
+        player = apiReply.getPlayer();
+        guild = guildReply.getGuild();
 
         if (!player.exists()) {
             System.err.println("Player Not Found");
-
-            api.shutdown();
             return;
         }
 
         System.out.println("[" + Util.formatRank(player.getHighestRank()) + "] " + player.getName() + "'s Stats");
-        System.out.println();
         System.out.println("UUID: " + player.getUuid());
         System.out.println("Guild Name: " + guild.getName());
+        System.out.println("Status: " + onlineStatus(ID));
+
+        try {
+            System.out.println("FakeNico UUID:" + getUUID("FakeNico"));
+        } catch (ExecutionException e) {
+            Fails.uuidGetFail();
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        getPlayerInfo();
+        getPlayerInfo(Util.FAKENICO);
+        shutDown();
     }
 }
